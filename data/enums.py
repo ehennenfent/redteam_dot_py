65a1463d257a3e8471da1c1358dc2896a519db26
Eric Hennenfent <ehennenfent@imsa.edu>
2018-04-14 17:11:51 -0500

    mr skeltal

---
+++ data/enums.py
@@ -0,0 +1,13 @@
+from enum import Enum, auto
+
+class OS(Enum):
+    Windows = auto()
+    Linux = auto()
+    Other = auto()
+
+class Arch(Enum):
+    x86 = auto()
+    x64 = auto()
+    ARM = auto()
+    MIPS = auto()
+    Other = auto()
