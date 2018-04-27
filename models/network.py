a01874c2c4a0ec673d7828119a7bbb21c05bc3ee
Eric Hennenfent <ehennenfent@imsa.edu>
2018-04-27 01:03:06 -0500

    screw it, I'll use sqlalchemy

--- models/network.py
+++ models/network.py
@@ -3,33 +3,26 @@ from typing import Dict, List
 from data.enums import Port
 
 class NAT(object):
-    
+
     def __init__(self):
-        
+
         self.router: Host = UnknownHost()
         self.tcp_map: Dict(int, Host) = {}
-        self.udp_map: Dict(int, Host) = {}
         self.hidden_hosts: List(Host) = []
 
-        
+
     @property
     def hosts(self):
-        return self.tcp_map.values() + self.udp_map.values()
-        
+        return self.tcp_map.values()
+
     @property
     def tcp_ports(self):
         return list(self.tcp_map.keys())
-        
-    @property
-    def udp_ports(self):
-        return list(self.udp_map.keys())
-        
-    def add_host(self, port: int, host: Host, t: Port = Port.tcp):
-        if t is Port.tcp:
-            self.tcp_map[port] = host
-        if t is Port.udp:
-            self.udp_map[port] = host
-    
+
+    def add_host(self, port: int, host: Host):
+        self.tcp_map[port] = host
+
+    # TODO support udp
     # TODO: support for hidden hosts - ones that don't expose anything to NAT
     # TODO: store the subnet
     # TODO: support getting hosts by ip address
